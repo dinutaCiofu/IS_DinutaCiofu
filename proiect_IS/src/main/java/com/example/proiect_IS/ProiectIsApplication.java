0203d6fbddce9df8193102ac3bd0201ac0b18bbe
dinutaCiofu <dinutaciofu@gmail.com>
2023-10-18 19:02:03 +0300

    proiect

---
+++ proiect_IS/src/main/java/com/example/proiect_IS/ProiectIsApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiect_IS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectIsApplication.class, args);
+	}
+
+}
